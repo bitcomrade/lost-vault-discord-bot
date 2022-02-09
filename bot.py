266e964e4c427d5a2935e1389d70ad8730e75d10
bitcomrade <bitcomrade@gmail.com>
2022-02-09 12:08:40 +0300

    + slash coomands cog activation

--- bot.py
+++ bot.py
@@ -1,25 +1,21 @@
-import nextcord
 import os
-import nextcord.ext
-# for testing purposes when hosting on my PC
-# Remove dotenv when deploy
-#from dotenv import load_dotenv
-#
+
+import nextcord
 from nextcord.ext import commands
-#^ basic imports for other features of nextcord.py and python ^
-
 
 # for testing purposes when hosting on my PC
 # Remove dotenv when deploy
+#from dotenv import load_doten
+
 #load_dotenv()
 
 # Set nextcord bot token variable according to your environment
 TOKEN = os.getenv("DISCORD-TOKEN")
-
 # Set command prefix below
 bot = commands.Bot(command_prefix = '!')
 
 bot.load_extension("basic_commands")
+bot.load_extension("application_commands")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
